b61110e49355356000418898e15c583174f3cb03
Mouhamedkane Ndongo <mouhamedkane54858@gmail.com>
2026-04-21 21:47:10 +0000

    Setup initial SenTransport avec Header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header'
+
+const App = () => (
+    <div className="App">
+      <Header />
+      <main className='contenue'>
+
+      <h1>Binevenue :) SenTransport  </h1>
+      <p>le plus grand reseau de transport en commu8n a dakar </p>
+      </main>
+    </div>
+  )
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export default function Header(){
+    return (
+        <header className='header'>
+        <h1 className="header-titre">SenTransport</h1>
+        <p className="header-soustitre" >Votre guide de transport en commun a dakar</p>
+
+        </header>
+    )
+}
