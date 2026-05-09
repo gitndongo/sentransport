450ed71c41dd59b9a96c0d1016bc6f3c43510d35
Mouhamedkane Ndongo <mouhamedkane54858@gmail.com>
2026-05-09 23:26:43 +0000

    Lab 3 : état, recherche et sélection de lignes (v2)

--- src/LigneBus.jsx
+++ src/LigneBus.jsx
@@ -1,9 +1,15 @@
 import './LigneBus.css';
 
-function LigneBus({ numero, depart, arrivee, arrets, couleur }) {
+function LigneBus({ numero, depart, arrivee, arrets, couleur, estSelectionnee, onClick }) {
   return (
-    <div className="ligne-bus">
-      <div className="ligne-numero" style={{ backgroundColor: couleur || '#0a6e31' }}>
+    <div
+      className={`ligne-bus ${estSelectionnee ? 'ligne-bus-active' : ''}`}
+      onClick={onClick}
+    >
+      <div
+        className="ligne-numero"
+        style={{ backgroundColor: estSelectionnee ? '#e67e22' : (couleur || '#0a6e31') }}
+      >
         {numero}
       </div>
       <div className="ligne-info">
